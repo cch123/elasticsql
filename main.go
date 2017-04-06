f28cb43d4fe680a71c18ef9078713ecc2655c8c0
Xargin <cao1988228@163.com>
2017-04-06 18:04:49 +0800

    add ConvertPretty func for elasticsql

--- main.go
+++ main.go
@@ -1,6 +1,28 @@
 package elasticsql
 
-import "github.com/xwb1989/sqlparser"
+import (
+	"bytes"
+
+	"encoding/json"
+
+	"github.com/xwb1989/sqlparser"
+)
+
+// ConvertPretty will transform sql to elasticsearch dsl, and prettify the output json
+func ConvertPretty(sql string) (dsl string, table string, err error) {
+	dsl, table, err = Convert(sql)
+	if err != nil {
+		return dsl, table, err
+	}
+
+	var prettifiedDSLBytes bytes.Buffer
+	err = json.Indent(&prettifiedDSLBytes, []byte(dsl), "", "  ")
+	if err != nil {
+		return "", table, err
+	}
+
+	return string(prettifiedDSLBytes.Bytes()), table, err
+}
 
 // Convert will transform sql to elasticsearch dsl string
 func Convert(sql string) (dsl string, table string, err error) {
